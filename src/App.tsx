d63b2c477cf67db92fcf3c2b019d3e2543ea6dbf
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-09-23 11:22:46 +0200

    Implemented code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import { checkUserSession } from "./store/user/user.action";
+import Navigation from './routes/navigation/navigation.component';
+
+import { checkUserSession } from './store/user/user.action';
+
+import Spinner from './components/spinner/spinner.component';
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Authentication = lazy(
+  () => import('./routes/authentication/authentication.component')
+);
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,14 +23,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
